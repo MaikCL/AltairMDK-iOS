1d6b627253d229f7f1edd3b511cff636aa974d30
Miguel Angel Zapata <mzapatae@icloud.com>
2021-05-05 17:29:35 -0400

    Add a method to make a bind and trigger an action

--- Sources/Common/Protocols/Store.swift
+++ Sources/Common/Protocols/Store.swift
@@ -2,6 +2,7 @@
 //  Created by Miguel Angel on 25-03-21.
 //
 
+import SwiftUI
 import Foundation
 
 public protocol Store: ObservableObject {
@@ -9,4 +10,15 @@ public protocol Store: ObservableObject {
     associatedtype Action
     var state: State { get }
     func trigger(_ action: Action)
+}
+
+public extension Store {
+    
+    func binding<Value>(for keyPath: KeyPath<State, Value>, transform: @escaping (Value) -> Action) -> Binding<Value> {
+        Binding<Value>(
+            get: { self.state[keyPath: keyPath] },
+            set: { self.trigger(transform($0)) }
+        )
+    }
+
 }
---
+++ Sources/Common/Varios.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  
+//
+//  Created by Miguel Angel on 07-05-21.
+//
+
+import Foundation
+import SwiftUI
+
