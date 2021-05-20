927b99a7724a8985545a2c4ebd8e69b096f41707
Miguel Angel Zapata <mzapatae@icloud.com>
2021-05-19 20:27:51 -0400

    Add variation to return the toggle result

---
+++ Sources/Common/Utils/Regex.swift
@@ -0,0 +1,25 @@
+//
+//  RegexMatch.swift
+//  
+//
+//  Created by Miguel Angel on 19-05-21.
+//
+
+import Foundation
+
+public final class Regex {
+
+    public static func matches(for regex: String, in text: String) -> [String] {
+        do {
+            let regex = try NSRegularExpression(pattern: regex)
+            let results = regex.matches(in: text, range: NSRange(text.startIndex..., in: text))
+            return results.map {
+                String(text[Range($0.range, in: text)!])
+            }
+        } catch let error {
+            print("Invalid Regex: \(error.localizedDescription)")
+            return []
+        }
+    }
+
+}
